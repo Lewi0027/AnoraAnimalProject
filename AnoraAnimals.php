954fd789a154877fa92bbf0442b5d9abf0574e43
Lewi0027 <lewi0027@yahoo.com>
2024-04-14 00:58:00 -0600

    updated mysql connection data

--- AnoraAnimals.php
+++ AnoraAnimals.php
@@ -2,29 +2,21 @@
 <html lang="en">
 <?php
 
-    try {
-        $dbh = new PDO('mysql:host=localhost;dbname=animals', 'root', 'RoadRunner66^^');
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+    $dsn = "mysql:host=localhost;dbname=mysql";
+    $username = "root";
+    $password = "RoadRunner66^^";
+    $pdo = new PDO($dsn, $username, $password);
+
+    //Throw exception if SQL error
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT * FROM animals;";
+
+    $statement = $pdo->query($sql);
+
+    while ($row = $statement->fetch()) {
+        echo "$row[animalType] - $row[habitat]\n";
     }
-
-    // $dsn = "mysql:host=localhost;dbname=mysql";
-    // $username = "Henry";
-    // $password = "RoadRunner66^^";
-    // $pdo = new PDO($dsn, $username, $password);
-
-    // //Throw exception if SQL error
-    // $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // $sql = "SELECT * FROM animals;";
-
-    // $statement = $pdo->query($sql);
-
-    // while ($row = $statement->fetch()) {
-    //     echo "$row[animalType] - $row[habitat]\n";
-    // }
 ?>
 <head>
     <meta charset="UTF-8">
