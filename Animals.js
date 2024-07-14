76f9d17f03331d7d56f732783ce0af750132d4bb
Lewi0027 <lewi0027@yahoo.com>
2024-07-13 22:30:54 -0600

    Added more animals

---
+++ Animals.js
@@ -0,0 +1,176 @@
+var animals = [
+    {
+        name: "Bear",
+        imageNum: 3,
+        sound: 'sound/bear.wav',
+        used: false
+    },
+    {
+        name: "Bee",
+        imageNum: 1,
+        sound: 'sound/bee.wav',
+        used: false
+    },
+    {
+        name: "Cat",
+        imageNum: 1,
+        sound: 'sound/cat.wav',
+        used: false
+    },
+    {
+        name: "Chimpanzee",
+        imageNum: 2,
+        sound: 'sound/chimpanzee.wav',
+        used: false
+    },
+    {
+        name: "Cow",
+        imageNum: 1,
+        sound: 'sound/cow.wav',
+        used: false
+    },
+    {
+        name: "Crow",
+        imageNum: 1,
+        sound: 'sound/crow.wav',
+        used: false
+    },
+    {
+        name: "Deer",
+        imageNum: 1,
+        sound: 'sound/deer.wav',
+        used: false
+    },
+    {
+        name: "Dog",
+        imageNum: 1,
+        sound: 'sound/dog.wav',
+        used: false
+    },
+    {
+        name: "Dolphin",
+        imageNum: 1,
+        sound: 'sound/dolphin.wav',
+        used: false
+    },
+    {
+        name: "Donkey",
+        imageNum: 1,
+        sound: 'sound/donkey.wav',
+        used: false
+    },
+    {
+        name: "Duck",
+        imageNum: 1,
+        sound: 'sound/duck.wav',
+        used: false
+    },
+    {
+        name: "Eagle",
+        imageNum: 1,
+        sound: 'sound/eagle.wav',
+        used: false
+    },
+    {
+        name: "Elephant",
+        imageNum: 1,
+        sound: 'sound/elephant.wav',
+        used: false
+    },
+    {
+        name: "Frog",
+        imageNum: 1,
+        sound: 'sound/frog.wav',
+        used: false
+    },
+    {
+        name: "Godzilla",
+        imageNum: 1,
+        sound: 'sound/godzilla.wav',
+        used: false
+    },
+    {
+        name: "Goat",
+        imageNum: 1,
+        sound: 'sound/goat.wav',
+        used: false
+    },
+    {
+        name: "Goose",
+        imageNum: 1,
+        sound: 'sound/goose.wav',
+        used: false
+    },
+    {
+        name: "Horse",
+        imageNum: 1,
+        sound: 'sound/horse.wav',
+        used: false
+    },
+    {
+        name: "Mouse",
+        imageNum: 1,
+        sound: 'sound/mouse.wav',
+        used: false
+    },
+    {
+        name: "Parrot",
+        imageNum: 1,
+        sound: 'sound/parrot.wav',
+        used: false
+    },
+    {
+        name: "Pig",
+        imageNum: 1,
+        sound: 'sound/pig.wav',
+        used: false
+    },
+    {
+        name: "Rooster",
+        imageNum: 1,
+        sound: 'sound/rooster.wav',
+        used: false
+    },
+    {
+        name: "Seal",
+        imageNum: 2,
+        sound: 'sound/seal.wav',
+        used: false
+    },
+    {
+        name: "Sheep",
+        imageNum: 1,
+        sound: 'sound/sheep.wav',
+        used: false
+    },
+    {
+        name: "Snake",
+        imageNum: 1,
+        sound: 'sound/snake.wav',
+        used: false
+    },
+    {
+        name: "Tiger",
+        imageNum: 1,
+        sound: 'sound/tiger.wav',
+        used: false
+    },
+    {
+        name: "Turkey",
+        imageNum: 1,
+        sound: 'sound/turkey.wav',
+        used: false
+    },
+    {
+        name: "Whale",
+        imageNum: 1,
+        sound: 'sound/whale.wav',
+        used: false
+    },
+    {
+        name: "Wolf",
+        imageNum: 1,
+        sound: 'sound/wolf.wav',
+        used: false
+    }
+];
--- AnoraAnimals.js
+++ AnoraAnimals.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     function imageStringGenerator(string, num) {
         var stringName = 'images/';
         stringName += string;
@@ -7,163 +8,15 @@ $(document).ready(function() {
         return stringName;
     }
 
-    // Array of animals
-    var animals = [
-        {
-            name: "Bear",
-            imageNum: 1,
-            sound: 'sound/bear.wav',
-            used: false
-        },
-        {
-            name: "Bee",
-            imageNum: 1,
-            sound: 'sound/bee.wav',
-            used: false
-        },
-        {
-            name: "Cat",
-            imageNum: 1,
-            sound: 'sound/cat.wav',
-            used: false
-        },
-        {
-            name: "Cow",
-            imageNum: 1,
-            sound: 'sound/cow.wav',
-            used: false
-        },
-        {
-            name: "Deer",
-            imageNum: 1,
-            sound: 'sound/deer.wav',
-            used: false
-        },
-        {
-            name: "Dog",
-            imageNum: 1,
-            sound: 'sound/dog.wav',
-            used: false
-        },
-        {
-            name: "Dolphin",
-            imageNum: 1,
-            sound: 'sound/dolphin.wav',
-            used: false
-        },
-        {
-            name: "Donkey",
-            imageNum: 1,
-            sound: 'sound/donkey.wav',
-            used: false
-        },
-        {
-            name: "Duck",
-            imageNum: 1,
-            sound: 'sound/duck.wav',
-            used: false
-        },
-        {
-            name: "Eagle",
-            imageNum: 1,
-            sound: 'sound/eagle.wav',
-            used: false
-        },
-        {
-            name: "Elephant",
-            imageNum: 1,
-            sound: 'sound/elephant.wav',
-            used: false
-        },
-        {
-            name: "Frog",
-            imageNum: 1,
-            sound: 'sound/frog.wav',
-            used: false
-        },
-        {
-            name: "Godzilla",
-            imageNum: 1,
-            sound: 'sound/godzilla.wav',
-            used: false
-        },
-        {
-            name: "Goat",
-            imageNum: 1,
-            sound: 'sound/goat.wav',
-            used: false
-        },
-        {
-            name: "Goose",
-            imageNum: 1,
-            sound: 'sound/goose.wav',
-            used: false
-        },
-        {
-            name: "Horse",
-            imageNum: 1,
-            sound: 'sound/horse.wav',
-            used: false
-        },
-        {
-            name: "Mouse",
-            imageNum: 1,
-            sound: 'sound/mouse.wav',
-            used: false
-        },
-        {
-            name: "Parrot",
-            imageNum: 1,
-            sound: 'sound/parrot.wav',
-            used: false
-        },
-        {
-            name: "Pig",
-            imageNum: 1,
-            sound: 'sound/pig.wav',
-            used: false
-        },
-        {
-            name: "Rooster",
-            imageNum: 1,
-            sound: 'sound/rooster.wav',
-            used: false
-        },
-        {
-            name: "Sheep",
-            imageNum: 1,
-            sound: 'sound/sheep.wav',
-            used: false
-        },
-        {
-            name: "Snake",
-            imageNum: 1,
-            sound: 'sound/snake.wav',
-            used: false
-        },
-        {
-            name: "Tiger",
-            imageNum: 1,
-            sound: 'sound/tiger.wav',
-            used: false
-        },
-        {
-            name: "Whale",
-            imageNum: 1,
-            sound: 'sound/whale.wav',
-            used: false
-        },
-        {
-            name: "Wolf",
-            imageNum: 1,
-            sound: 'sound/wolf.wav',
-            used: false
-        }
-    ];
+    animals.forEach(function(animal) {
+        animal.randomNum = Math.floor(Math.random() * animal.imageNum) + 1;
+    });
 
     animals.forEach(function(animal) {
-        animal.image = imageStringGenerator(animal.name.toLowerCase(), animal.imageNum);
+        animal.image = imageStringGenerator(animal.name.toLowerCase(), animal.randomNum);
     });
+
+    console.log(animals);
 
     var selectedAnimals;
     var currentIndex;
